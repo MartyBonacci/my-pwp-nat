830867a8ea57a1fa0f25bc67540df3946b092b9a
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-12 11:54:20 -0700

    started developing the persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -11,9 +11,8 @@
 	 <section class="purpose">
 		 <h2>Purpose, Audience & Goal</h2>
 		 <p>Purpose: This project will be my personal, professional website with a fouc</p>
-		 <p>Audience: This site is designed with small business owners in mind. It is targeted at individuals in the local community that need fully functional web solutions for their business. </p>
-		 <p>Goal: The main goal of this website is for visitors to the site </p>
-	 Goal: The primary goal of this site is for users of the site to initiate contact with me in regards to employment and collaboration opportunities through the contact form, and/or the connected social media profiles. The secondary goal of this site is to showcase my work, and to contribute to a polished professional online presence.
+		 <p>Audience: This site is designed with small business owners in mind. It is targeted at individuals/businesses in the local community that need fully functional web solutions for their business.</p>
+		 <p>Goal: The main goal of this website is for visitors to be able to reach out to me for work oppurtunities or collaborations. My site will have a contact form that they can use as well as links to my social media profiles. </p>
 	 </section>
 	 <section class="persona">
 	 Persona
@@ -26,11 +25,11 @@
 
 	 Attitudes and Behaviors: Jillian cares about community and the vibe she puts out into world. She wants her website to reflect that and foster a safe, positive environment for people to voice their concerns. She is active on Facebook, Instagram and Twitter.
 
-	 Frustrations and Needs: David needs to hire someone with 5-7 years of development experience who is willing to start at a competitive entry-level salary, to start. David is also looking for someone who is a good personality and cultural fit for FashionGun as well. FashionGun is still small, and can only offer limited benefits at this time, but will include equity options as part of the compensation package. Because David is looking to hire within a very competitive urban market, he is flooded with candidates but is having a hard time finding someone with the right combination of talent, experience, and personality who is willing to accept a slightly lower salary and limited benefits. He must hire within 2 weeks.
+	 Frustrations and Needs: Jillian needs to hire someone that can create a fully-functioning website and will be able to update the website as her business expands. She currently has a website that she created on her own, however she is spending too much of her time trying to fix and format the website and not enough time on her actual business.
 
-	 Goals: David is only concerned with one thing: Is this person a potential candidate for the FashionGun Web Dev position? Can David, within 3 minutes max, determine whether this person is worthy of a preliminary phone screen regarding the job? David doesn't like to waste time. David needs to know via a very fast interaction with the site whether or not this potential candidate has the minimum qualifications to warrant a call.
+	 Goals:
 
-	 User Story: As a user, I need to determine quickly whether this person is a candidate for the mid-level Front End Web Developer position.
+	 User Story: As a user, Jillian needs to
 
 	 </section>
  </body>
