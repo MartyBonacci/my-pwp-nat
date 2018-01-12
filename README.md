8edefd367a07ba1be22512a7f55f443d30c046a7
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-11 18:53:54 -0700

    started developing the persona

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # my-pwp-nat
 My PWP Repository:
 This repository is used to store my personal website and the supporting documentation.
+<
--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,7 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: nathanielgustafson
- * Date: 1/11/18
- * Time: 12:57 PM
- */
+
+<!DOCTYPE html>
+<html lang="en"/>
+
+<head>
+	<title>Nathaniel Gustafson's Pro Dev Doc</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="">
+</head>
+ <body>
+	 <section class="purpose">
+		 <h2>Purpose, Audience & Goal</h2>
+		 <p>Purpose: This project will be my personal, professional website with a fouc</p>
+		 <p>Audience: This site is designed with small business owners in mind. It is targeted at individuals in the local community that need fully functional web solutions for their business. </p>
+		 <p>Goal: The main goal of this website is for visitors to the site </p>
+	 Goal: The primary goal of this site is for users of the site to initiate contact with me in regards to employment and collaboration opportunities through the contact form, and/or the connected social media profiles. The secondary goal of this site is to showcase my work, and to contribute to a polished professional online presence.
+	 </section>
+	 <section class="persona">
+	 Persona
+	 Name: Jillian Parkhurst
+	 Age: 37
+
+	 Profession: Founder of Fresh Things First, a nutrition and cooking business that specializes in personal diet plans and cooking classes catered to people with a variety of dietary restrictions.
+
+	 Technology: Mac user. Uses a MacBook Pro at her home office as well as an iPhone 7 with plenty of data.
+
+	 Attitudes and Behaviors: Jillian cares about community and the vibe she puts out into world. She wants her website to reflect that and foster a safe, positive environment for people to voice their concerns. She is active on Facebook, Instagram and Twitter.
+
+	 Frustrations and Needs: David needs to hire someone with 5-7 years of development experience who is willing to start at a competitive entry-level salary, to start. David is also looking for someone who is a good personality and cultural fit for FashionGun as well. FashionGun is still small, and can only offer limited benefits at this time, but will include equity options as part of the compensation package. Because David is looking to hire within a very competitive urban market, he is flooded with candidates but is having a hard time finding someone with the right combination of talent, experience, and personality who is willing to accept a slightly lower salary and limited benefits. He must hire within 2 weeks.
+
+	 Goals: David is only concerned with one thing: Is this person a potential candidate for the FashionGun Web Dev position? Can David, within 3 minutes max, determine whether this person is worthy of a preliminary phone screen regarding the job? David doesn't like to waste time. David needs to know via a very fast interaction with the site whether or not this potential candidate has the minimum qualifications to warrant a call.
+
+	 User Story: As a user, I need to determine quickly whether this person is a candidate for the mid-level Front End Web Developer position.
+
+	 </section>
+ </body>
+</html>
