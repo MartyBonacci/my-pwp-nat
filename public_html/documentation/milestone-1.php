a439a21cd3263e35ac61094cce787eb74e46652c
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-14 12:00:49 -0700

    created css page

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,31 +5,31 @@
 <head>
 	<title>Nathaniel Gustafson's Pro Dev Doc</title>
 	<meta charset="utf-8" />
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="main.css"/>
 </head>
  <body>
 	 <section class="purpose">
-		 <h2>Purpose, Audience & Goal</h2>
-		 <p>Purpose: This project will be my personal, professional website with a fouc</p>
+		 <h2>Purpose, Audience & Goals</h2>
+		 <p>Purpose: This project will be my personal, professional website with a focus on showing that I can build tasteful, interesting websites.</p>
 		 <p>Audience: This site is designed with small business owners in mind. It is targeted at individuals/businesses in the local community that need fully functional web solutions for their business.</p>
 		 <p>Goal: The main goal of this website is for visitors to be able to reach out to me for work oppurtunities or collaborations. My site will have a contact form that they can use as well as links to my social media profiles. </p>
 	 </section>
 	 <section class="persona">
-	 Persona
-	 Name: Jillian Parkhurst
-	 Age: 37
-
-	 Profession: Founder of Fresh Things First, a nutrition and cooking business that specializes in personal diet plans and cooking classes catered to people with a variety of dietary restrictions.
-
-	 Technology: Mac user. Uses a MacBook Pro at her home office as well as an iPhone 7 with plenty of data.
-
-	 Attitudes and Behaviors: Jillian cares about community and the vibe she puts out into world. She wants her website to reflect that and foster a safe, positive environment for people to voice their concerns. She is active on Facebook, Instagram and Twitter.
-
-	 Frustrations and Needs: Jillian needs to hire someone that can create a fully-functioning website and will be able to update the website as her business expands. She currently has a website that she created on her own, however she is spending too much of her time trying to fix and format the website and not enough time on her actual business.
-
-	 Goals:
-
-	 User Story: As a user, Jillian needs to
+		 <h2>Persona</h2>
+		 <p>Name: Jillian Parkhurst <br>
+		 Age: 37
+			<br>
+		 Profession: Founder of Fresh Things First, a nutrition and cooking business that specializes in personal diet plans and cooking classes catered to people with a variety of dietary restrictions.
+			<br>
+		 Technology: Mac user. Uses a MacBook Pro at her home office as well as an iPhone 7 with plenty of data.
+			<br>
+		 Attitudes and Behaviors: Jillian cares about community and the vibe she puts out into world. She wants her website to reflect that and foster a safe, positive environment for people to voice their concerns. She is active on Facebook, Instagram and Twitter.
+			<br>
+		 Frustrations and Needs: Jillian needs to hire someone that can create a fully-functioning website and will be able to update the website as her business expands. She currently has a website that she created on her own, however she is spending too much of her time trying to fix and format the website and not enough time on her actual business.
+			<br>
+		 Goals: Jillian would like to hire someone that can rebuild her site in a timely fashio without much direction from her.
+			<br>
+		 User Story: As a user, Jillian needs to see my site, quickly gauge if she wants to pursue working with me, and easily contact me through my website.
 
 	 </section>
  </body>
