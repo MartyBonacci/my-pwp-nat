dbb215930e31789f0fc924dfff9300f3fa326b06
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-15 19:44:56 -0700

    changed my gameplan to create a site for my Mom's yoga practice

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="en"/>
+<html lang="en">
 
 <head>
 	<title>Nathaniel Gustafson's Pro Dev Doc</title>
@@ -8,32 +8,7 @@
 	<link rel="stylesheet" href="main.css"/>
 </head>
  <body>
-	 <h1>Milestone 1 Version 1: My Professional Page</h1>
-	 <section class="purpose">
-		 <h2>Purpose, Audience & Goals</h2>
-		 <p><em>Purpose:</em> This project will be my professional page with a focus on showing that I can build attractive, interesting websites.</p>
-		 <p><em>Audience:</em> This site is designed with small business owners in mind. It is targeted at individuals/businesses in the local community that need fully functional web solutions for their business.</p>
-		 <p><em>Goal:</em> The main goal of this website is for visitors to be able to reach out to me for work opportunities or collaborations. My site will have a contact form that they can use as well as links to my social media profiles. </p>
-	 </section>
-	 <section class="persona">
-		 <h2>Persona</h2>
-		 <p><em>Name:</em> Jillian Parkhurst <br>
-			 <em>Age:</em> 37
-			<br>
-			 <em>Profession:</em> Founder of Fresh Things First, a nutrition and cooking business that specializes in personal diet plans and cooking classes catered to people with a variety of dietary restrictions.
-			<br>
-		<em>Technology:</em>  Mac user. Uses a MacBook Pro at her home office as well as an iPhone 7 with plenty of data.
-			<br>
-		 <em>Attitudes and Behaviors: </em>Jillian cares about community and the vibe she puts out into world. She wants her website to reflect that and foster a safe, positive environment for people to voice their concerns. She is active on Facebook, Instagram and Twitter.
-			<br>
-			 <em>Frustrations and Needs:</em> Jillian needs to hire someone that can create a fully-functioning website and will be able to update the website as her business expands. She currently has a website that she created on her own, however she is spending too much of her time trying to fix and format the website and not enough time on her actual business.
-			<br>
-		 <em>Goals: </em>Jillian would like to hire someone that can rebuild her site in a timely fashio without much direction from her.
-			<br>
-			 <em>User Story: </em>As a user, Jillian needs to see my site, quickly gauge if she wants to pursue working with me, and easily contact me through my website.
-
-	 </section>
-	 <h1>Milestone 1 Version 2: Sarah Gustafson's Yoga Page</h1>
+	 <h1>Milestone 1: Sarah Gustafson's Yoga Page</h1>
 	 <section class="purpose">
 		 <h2>Purpose, Audience & Goals</h2>
 		 <p><em>Purpose:</em> The purpose of this project is to create a website showing Sarah's yoga style, credentials, and schedule.</p>
@@ -47,16 +22,15 @@
 			 <br>
 			 <em>Profession:</em> Lisa works for Los Alamos High School. She is a biology teacher and mother of 2.
 			 <br>
-			 <em>Technology:</em>  At work she uses a Dell Desktop running Windows 10. At home she uses a Macbook Pro and an iPhone 7.
+			 <em>Technology:</em>  At work she uses a Dell Desktop running Windows 10. At home she uses a Macbook Pro and an iPhone 7. She does most of her web searches on her work computer or phone.
 			 <br>
 			 <em>Attitudes and Behaviors: </em> Lisa is a busy women! She has to balance her career as a teacher with taking care of her two children. Lisa used to be very physically active, but over the years has had less time to excercise. A recent back injury has motivated her to focus more on her health.
 			 <br>
-			 <em>Frustrations and Needs:</em> Lisa has been frustrated with the other yoga classes she has found in her community. The other classes she has been to have been too big, and she worried that she was not doing the positions in a safe way for her recovering back. She would like to have more personal attention in the class to make sure that she is using correct technique.
+			 <em>Frustrations and Needs:</em> Lisa has been frustrated with the other yoga classes she has found in her community. The other classes she has been to have been too big, and she worried that she was not doing the positions in a safe way for her recovering back. She would like to have more personal attention in the class to make sure that she is using correct technique. Additionally, there is a lack of quality web resources to find out about options in her area. She is busy and only has chances to do quick searches on the web.
 			 <br>
 			 <em>Goals: </em> Lisa would like to find a yoga teacher to give her private classes to help her continue to learn as a yogi in a way that is safe for her lower back.
 			 <br>
 			 <em>User Story: </em> As a user, Lisa needs to see Sarah's site, quickly gauge if Sarah is a teacher that Lisa would like to take classes from, and have a way of contacting Sarah.
-
 	 </section>
  </body>
 </html>
---
+++ public_html/documentation/old-milestone-1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Original Milestone 1</title>
+	</head>
+	<body>
+		<h1>Milestone 1 Version 1: My Professional Page</h1>
+		<section class="purpose">
+			<h2>Purpose, Audience & Goals</h2>
+			<p><em>Purpose:</em> This project will be my professional page with a focus on showing that I can build attractive, interesting websites.</p>
+			<p><em>Audience:</em> This site is designed with small business owners in mind. It is targeted at individuals/businesses in the local community that need fully functional web solutions for their business.</p>
+			<p><em>Goal:</em> The main goal of this website is for visitors to be able to reach out to me for work opportunities or collaborations. My site will have a contact form that they can use as well as links to my social media profiles. </p>
+		</section>
+		<section class="persona">
+			<h2>Persona</h2>
+			<p><em>Name:</em> Jillian Parkhurst<br>
+				<em>Age:</em> 37
+				<br>
+				<em>Profession:</em> Founder of Fresh Things First, a nutrition and cooking business that specializes in personal diet plans and cooking classes catered to people with a variety of dietary restrictions.
+				<br>
+				<em>Technology:</em>  Mac user. Uses a MacBook Pro at her home office as well as an iPhone 7 with plenty of data.
+				<br>
+				<em>Attitudes and Behaviors: </em>Jillian cares about community and the vibe she puts out into world. She wants her website to reflect that and foster a safe, positive environment for people to voice their concerns. She is active on Facebook, Instagram and Twitter.
+				<br>
+				<em>Frustrations and Needs:</em> Jillian needs to hire someone that can create a fully-functioning website and will be able to update the website as her business expands. She currently has a website that she created on her own, however she is spending too much of her time trying to fix and format the website and not enough time on her actual business.
+				<br>
+				<em>Goals: </em>Jillian would like to hire someone that can rebuild her site in a timely fashio without much direction from her.
+				<br>
+				<em>User Story: </em>As a user, Jillian needs to see my site, quickly gauge if she wants to pursue working with me, and easily contact me through my website.
+
+		</section>
+	</body>
+</html>
