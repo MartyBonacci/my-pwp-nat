24b46bf295d989a196d5b155ee3ba302e3dc1d5e
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-20 23:16:41 -0700

    worked on milestone2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,32 @@
+
+/**
+ *@author Nat Gustafson <natjgus@gmail.com>
+ *this is my Milestone 2 documentation including my wireframes and content strategy
+ **/
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Nathaniel Gustafson's Pro Dev Doc</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="css/main.css"/>
+</head>
+ <body>
+	 <h1>Milestone 1: Sarah Gustafson's Yoga Page</h1>
+	 <section class="purpose">
+		 <h2>Content Strategy</h2>
+		 <p> My site will be one page, and will consist of the following sections:</p>
+		 <br>
+		 <p>Welcome: A brief one-line introduction that clearly states Sarah and Yoga Instructor. This welcome message will be overlaid on a large background image either pretty nature shot or yoga shot .
+			<br>
+			 About Sarah: This section will detail her professional history and a bit of her personal backstory. This will create and emotional connection with the viewer.
+		 	<br>
+		 	Below the About Sarah section will be her headshot (maybe this will be expandable from a smaller photo inline with the text.
+		 	<br>
+		 	Yoga Accomplishments: This section will highlight Sarah's teaching credentials and yoga achievements.
+			<br>
+			 Calendar: This section will be a calendar view of Sarah's weekly recurring classes.
+			<br>
+			 Contact: This section will contain a simple contact form, with Sarah's details: email and phone as well as a message box to send a message directly on the site.
+			</p>
