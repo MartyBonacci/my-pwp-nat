fd2bae9b2da4ebba83399d36d4b1c1950c74509d
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-27 19:07:57 -0700

    updated the content strategy.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,18 +9,16 @@
 	<link rel="stylesheet" href="css/main.css"/>
 </head>
  <body>
-	 <h1>Milestone 1: Sarah Gustafson's Yoga Page</h1>
+	 <h1>Milestone 2: Sarah Gustafson's Yoga Page</h1>
 	 <section class="purpose">
 		 <h2>Content Strategy</h2>
 		 <p> My site will be one page, and will consist of the following sections:</p>
 		 <br>
-		 <p><b>Welcome:</b> A brief one-line introduction that clearly states Sarah and Yoga Instructor. This welcome message will be overlaid on a large background image either pretty nature shot or yoga shot .
+		 <p><b>Welcome:</b> A brief one-line introduction that clearly states Sarah and Yoga Instructor. This welcome message will be overlaid on a large background image either pretty nature shot or yoga shot. The tope of the page will have a nav bar to jump around the site.
 			<br>
-			 <b>About Sarah:</b> This section will detail her professional history and a bit of her personal backstory. This will create and emotional connection with the viewer.
+			 <b>About Sarah:</b> This section will detail her professional yoga history and a bit of her personal backstory. This will create and emotional connection with the viewer. THe text will be broken up by Sarah's headshot.
 		 	<br>
-		 	Below the About Sarah section will be her headshot (maybe this will be expandable from a smaller photo inline with the text.
-		 	<br>
-			 <b>Yoga Accomplishments:</b> This section will highlight Sarah's teaching credentials and yoga achievements.
+			 <b>About Sarah's Classes:</b> This section will highlight foucs on Sarah's group and private yoga classes. They will describe the style and what ype of yogi they are geared towards. Between the two class types (Private and Group) there will be an expandable photo gallery with some photos from her different classes.
 			<br>
 			 <b>Calendar:</b> This section will be a calendar view of Sarah's weekly recurring classes.
 			<br>
