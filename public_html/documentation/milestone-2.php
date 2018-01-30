2b15be3d36ee895bc8c54ce1649a51ae1b285853
Nathaniel Gustafson <natjgus@gmail.com>
2018-01-30 15:44:39 -0700

    updated the content strategy.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -14,9 +14,9 @@
 		 <h2>Content Strategy</h2>
 		 <p> My site will be one page, and will consist of the following sections:</p>
 		 <br>
-		 <p><b>Welcome:</b> A brief one-line introduction that clearly states Sarah and Yoga Instructor. This welcome message will be overlaid on a large background image either pretty nature shot or yoga shot. The tope of the page will have a nav bar to jump around the site.
+		 <p><b>Welcome:</b> A brief one-line introduction that clearly states Sarah and Yoga Instructor. This site will be showcasing my photography skills as well so this will be a high quality image. On the mobile view this photo will take up the whole screen (possibly it will be a different photo that is not a wide landscape. On the full-screen layout the photo will be front and center with a nav
 			<br>
-			 <b>About Sarah:</b> This section will detail her professional yoga history and a bit of her personal backstory. This will create and emotional connection with the viewer. THe text will be broken up by Sarah's headshot.
+			 <b>About Sarah:</b> This section will detail her professional yoga history and a bit of her personal backstory. This will create and emotional connection with the viewer. The text will be broken up by Sarah's headshot.
 		 	<br>
 			 <b>About Sarah's Classes:</b> This section will highlight foucs on Sarah's group and private yoga classes. They will describe the style and what ype of yogi they are geared towards. Between the two class types (Private and Group) there will be an expandable photo gallery with some photos from her different classes.
 			<br>
