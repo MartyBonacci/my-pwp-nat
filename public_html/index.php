e0a4a33dbdc03e36e5f44f63a164d8cc43ee7514
Nathaniel Gustafson <natjgus@gmail.com>
2018-02-07 20:44:03 -0700

    started on the nav bar. I should start elsewhere!

--- public_html/index.php
+++ public_html/index.php
@@ -17,5 +17,24 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </head>
-
  <body>
+	 <div class="container">
+		 <nav class="navbar navbar-expand-lg navbar-toggleable-md navbar-light bg-light border rounded">
+			 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				 <span class="navbar-toggler-icon"></span>
+			 </button>
+			 <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+				 <div class="navbar-nav">
+					 <a class="nav-tem nav-link" href="#">About Sarah</a>
+					 <a class="nav-tem nav-link" href="#">Private Classes</a>
+					 <a class="nav-tem nav-link" href="#">Group Classes and Schedule</a>
+					 <a class="nav-tem nav-link" href="#">Contact</a>
+				 </div>
+			 </div>
+		 </nav>
+	 </div>
+		 </nav>
+	 </div>
+
+ </body>
+</html>
